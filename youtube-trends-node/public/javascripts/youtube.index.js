7b688553d21ebc850e68d910a70cc7d95b3b8c3b
Igor J. Santos <igor@getdigital.com.br>
2018-08-13 23:24:33 +1000

    add a loader animation

--- youtube-trends-node/public/javascripts/youtube.index.js
+++ youtube-trends-node/public/javascripts/youtube.index.js
@@ -1,11 +1,15 @@
 (function () {
 
   const prepareFilter = () => {
+    const loader = document.querySelector('.loader');
+    const videoList = document.querySelector('#youtube');
     const countrySelectFilter = document.querySelector('#country-select');
 
     countrySelectFilter.addEventListener('change', (event) => {
       const regionCode = event.target.selectedOptions[0].value;
       document.location.search = `?regionCode=${regionCode}`;
+      videoList.classList.add('hidden');
+      loader.classList.remove('hidden');
     }, false);
   };
 
